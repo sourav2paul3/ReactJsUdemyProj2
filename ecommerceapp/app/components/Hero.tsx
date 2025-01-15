6936a3e96d59d1f05946afa745498c6d31b64afd
sourav2paul3 <40515551+sourav2paul3@users.noreply.github.com>
2025-01-15 15:12:05 +0530

    Hero

--- ecommerceapp/app/components/Hero.tsx
+++ ecommerceapp/app/components/Hero.tsx
@@ -1,10 +1,27 @@
+"use client";
 import React from "react";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import heroAirpods from "@/public/heroAirpods.png";
+import heroBanner from "@/public/heroBanner.png";
 
 const Hero = () => {
+  const variants = {
+    hidden: { x: 0, y: 70, opacity: 0 },
+    visible: { x: 0, y: -10, opacity: 1, transition: { delay: 0.05 } },
+  };
+
   return (
-    <div className="w-full h-[600px] flex border">
-      <div className="h-full md:max-w-[1024px] max-w-[600px] m-auto">
-        <div className="object-cover"> </div>
+    <div className="hero-section">
+      <div className="hero-container">
+        <div className="object-cover">
+          <Image src={heroBanner} height={100} width={1400} alt="banner" />
+        </div>
+        <div className="hero-airpods">
+          <motion.div initial="hidden" animate="visible" variants={variants}>
+            <Image src={heroAirpods} height={100} width={700} alt="airpods" />
+          </motion.div>
+        </div>
       </div>
     </div>
   );
---
+++ ecommerceapp/public/index.ts
@@ -0,0 +1,4 @@
+import heroAirpods from "./heroAirpods.png";
+import heroBanner from "./heroBanner.png";
+
+export { heroAirpods, heroBanner };
