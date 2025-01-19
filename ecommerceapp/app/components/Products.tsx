8ca0badd8d04ff57a98cc9355ca70baacc448027
sourav2paul3 <40515551+sourav2paul3@users.noreply.github.com>
2025-01-19 13:50:33 +0530

    Prod cards

--- ecommerceapp/app/components/Products.tsx
+++ ecommerceapp/app/components/Products.tsx
@@ -6,13 +6,13 @@ import { urlForImage } from "@/sanity/lib/image";
 const Products = async () => {
   const products = await client.fetch(groq`*[_type=="product"]`);
   return (
-    <div className="bg-[#f8f8f8] w-full py-12 mt-[100px]">
+    <div className="bg-[#f8f8f8] w-full py-12 mt-[125px]">
       <div className="container">
-        <div>
+        <div className="py-4">
           <h1 className="text-3xl font-bold">Best Selling Products</h1>
-          <h1 className="py-4"> Enjoy upto 50%</h1>
+          <h1> Enjoy upto 50%</h1>
         </div>
-        <div className="mt-[6px] ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 mt-6">
           {products.map((product: any, index: number) => (
             <div
               key={index}
@@ -20,10 +20,17 @@ const Products = async () => {
             >
               <Image
                 src={urlForImage(product.images && product.images[0]).url()}
-                alt="Product Image"
-                width={500}
-                height={500}
+                alt={product.slug}
+                width={220}
+                height={100}
+                className="object-cover h-32 mx-auto"
               />
+              <div className="text-center py-10">
+                <h1 className="text-2xl font-bold">{product.name}</h1>
+                <h1 className="text-xl text-gray-500 font-bold">
+                  ${product.price}
+                </h1>
+              </div>
             </div>
           ))}
         </div>
