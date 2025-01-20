7bc1dbd579cc2b4f3b23508f51e687afe9448f0b
sourav2paul3 <40515551+sourav2paul3@users.noreply.github.com>
2025-01-20 11:56:45 +0530

    prod details

--- ecommerceapp/app/Product/[slug]/page.tsx
+++ ecommerceapp/app/Product/[slug]/page.tsx
@@ -1,18 +1,19 @@
-"use client";
-import { useParams } from "next/navigation";
 import { ProductsDetails } from "../../components";
 import { Navbar } from "../../components";
 import React from "react";
 import { groq } from "next-sanity";
 import { client } from "@/sanity/lib/client";
 
-const page = async () => {
-  const { slug: any } = useParams();
+const page = async ({ params }: { params: { slug: string } }) => {
   const products = await client.fetch(groq`*[_type=="product"]`);
+  const currentProd = products.find(
+    (prod: any) => prod.slug.current === params.slug
+  );
+  console.log(currentProd);
   return (
     <>
       <Navbar />
-      <ProductsDetails />
+      <ProductsDetails product={currentProd} />
     </>
   );
 };
--- ecommerceapp/app/components/ProductsDetails.tsx
+++ ecommerceapp/app/components/ProductsDetails.tsx
@@ -1,8 +1,76 @@
+"use client";
+import { urlForImage } from "@/sanity/lib/image";
+import Image from "next/image";
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
-import React from "react";
+type Product = {
+  name: string;
+  price: number;
+  description: string;
+  _id: string;
+  images: Array<{ url: string }>;
+  slug: { current: string };
+};
 
-const ProductsDetails = () => {
-  return <div></div>;
+const ProductsDetails = ({ product }: { product: Product }) => {
+  const [index, setIndex] = useState<number>(0);
+  const [quantity, setQuantity] = useState<number>(1);
+  return (
+    <div className="product-details-section">
+      <div className="product-details-container">
+        {/*Left*/}
+        <div>
+          {/*Top*/}
+          <div className="h-450 flex items-center mb-25px">
+            <Image
+              src={urlForImage(product.images[index]).url()}
+              alt={product.name}
+              height={350}
+              width={450}
+              className="object-cover mx-auto"
+            />
+          </div>
+          {/*Bottom*/}
+          <div className="small-images-container">
+            {product.images.map((image: { url: string }, i: number) => (
+              <Image
+                key={i}
+                src={urlForImage(product.images[i]).url()}
+                alt={`Product image ${i + 1}`}
+                height={100}
+                width={220}
+                className="object-cover h-32 mx-auto border rounded-xl hover:cursor-pointer"
+                onClick={() => {
+                  setIndex(i);
+                }}
+              />
+            ))}
+          </div>
+        </div>
+        {/*Right*/}
+        <div className="flex flex-col gap-8 md:pt-32 pt-0">
+          <div className="flex flex-col gap-4">
+            <div className="text-3xl font-bold">{product.name}</div>
+            <div className="text-xl font-medium">${product.price}</div>
+          </div>
+          <div className="flex gap-2 items-center">
+            <h3>Quantity</h3>
+            <p className="quantity-desc flex items-center border-black">
+              <span className="minus" onClick={() => setQuantity(quantity - 1)}>
+                <AiOutlineMinus />
+              </span>
+              <span className="num">{quantity}</span>
+              <span className="plus" onClick={() => setQuantity(quantity + 1)}>
+                <AiOutlinePlus />
+              </span>
+            </p>
+          </div>
+          <button className="btn add-to-cart ">Add To Cart</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductsDetails;
